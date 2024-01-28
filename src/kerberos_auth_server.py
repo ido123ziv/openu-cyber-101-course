0b337a46c2397f89096fb9137226b6021a7a7c40
idosh <idoshziv@gmail.com>
2024-01-28 22:37:29 +0200

    support multi lines and clients

--- src/kerberos_auth_server.py
+++ src/kerberos_auth_server.py
@@ -6,6 +6,7 @@ import base64
 from Crypto.Random import get_random_bytes
 from Crypto.Hash import SHA256
 from uuid import uuid1
+
 CLIENT_FILE = "clients.info"
 # todo use multiple message services for now leave it
 SERVERS_FILE = "servers.info"
@@ -31,6 +32,7 @@ class KerberosAuthServer:
     """
     This class represents an auth server used by the kerberos protocol and handles all the commands
     """
+
     def __init__(self):
         """
         creates an object
@@ -81,7 +83,6 @@ class KerberosAuthServer:
         :return: dict of current message server info
         """
         return self._message_sever
-
 
     # TODO add a parsing to the client names
     def get_clients_names(self):
@@ -150,21 +151,36 @@ class KerberosAuthServer:
         #     client_request = self.receive_client_request()
         #     if client_request:
         #         pass
-                # TODO add support for multi servers using threads
-                # thread = threading.Thread(target=self.handle_client_request, args=(client_request,))
-                # thread.start()
+        # TODO add support for multi servers using threads
+        # thread = threading.Thread(target=self.handle_client_request, args=(client_request,))
+        # thread.start()
 
 
 def load_clients():
+    # TODO: make it better than addressing the locations hard coded
     """
     Loads client from file
     :return: a list of give clients
     """
     try:
         with open(CLIENT_FILE, 'r') as clients_file:
-            clients = clients_file.readlines()
-            if clients:
-                return clients
+            clients_list = clients_file.readlines()
+            if clients_list is None or not clients_list:
+                raise LookupError
+            clients = []
+            for row in clients_list:
+                """
+                parsing this:
+                ID: 219612343443330567787200566001537885281 Name: alice PasswordHash: 8a5eba0ab714cbcd4f314334f073c446c3092192de2e40271203a722f41648a5 LastSeen: 2024-01-28 22:34:33
+                """
+                client = row.split(" ")
+                clients.append({
+                    "ID": client[1],
+                    "Name": client[3],
+                    "PasswordHash": client[5],
+                    "LastSeen": client[7] + " " + client[8].strip()
+                })
+            return clients
     except Exception as e:
         print(str(e))
         print("No clients found")
@@ -180,12 +196,17 @@ def add_client_to_file(clients):
     try:
         with open(CLIENT_FILE, 'w+') as clients_file:
             for client in clients:
-                clients_file.write(client + "\n")
+                # clients_file.write(client + "\n")
+                clients_file.write("ID: " + client.get("ID"))
+                clients_file.write(" Name: " + client.get("Name"))
+                clients_file.write(" PasswordHash: " + client.get("PasswordHash"))
+                clients_file.write(" LastSeen: " + client.get("LastSeen") + "\n")
     except Exception as e:
         print(str(e))
         print("Couldn't add client, defaulting to previous state")
         with open(CLIENT_FILE, 'w') as clients_file:
-            clients_file.writelines(backup_client)
+            if backup_client is not None or backup_client != []:
+                clients_file.writelines(backup_client)
 
 
 def main():
