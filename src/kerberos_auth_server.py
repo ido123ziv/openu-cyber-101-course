89a5b936c1323adc026fbe8d4867854c39476c71
idosh <idoshziv@gmail.com>
2024-01-30 00:04:55 +0200

    adding key generating

--- src/kerberos_auth_server.py
+++ src/kerberos_auth_server.py
@@ -1,6 +1,9 @@
 import json
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
+
+from Crypto.Util.Padding import pad, unpad
+
 # import threading
 from shared_server import *
 import base64
@@ -105,38 +108,64 @@ class KerberosAuthServer:
 
     # TODO add a parsing to the client names
     def get_clients_names(self):
+        """
+        getting a list of names from current client list
+        :return: a list of all client names
+        """
         return [x["Name"] for x in self.clients]
 
-    # TODO: check if needed
-    def generate_salt(self):
-        """
-        :return: salt ?
-        """
-        return get_random_bytes(16)
+    # # TODO: check if needed
+    # def generate_salt(self):
+    #     """
+    #     :return: salt ?
+    #     """
+    #     return get_random_bytes(16)
 
-    def generate_session_key(self, client_id):
+    def encrypt_aes(self, aes_key, nonce, data):
         """
 
-        :param client_id:
+        :param aes_key: AES Symmetric Key in used
+        :param data: data to encrypt
+        :param nonce: random value created by the client
         :return:
+        """
+        ciphertext = aes_key.encrypt(pad(data, AES.block_size))
+        return base64.b64encode(nonce + ciphertext)
+
+    def decrypt_aes(self, encrypted_data, key):
+        data = base64.b64decode(encrypted_data)
+        iv, ciphertext = data[:16], data[16:]
+        cipher = AES.new(key, iv, AES.MODE_CBC)
+        decrypted_data = unpad(cipher.decrypt(ciphertext), AES.block_size)
+        return decrypted_data
+
+    def generate_session_key(self, client_id, server_id, nonce):
+        """
+        :param nonce: random value created by the client
+        :param server_id: messaging server id
+        :param client_id: client id of user initiated the request
+        :return: a tuple of AES Key and ticket encrypted
         """
         clients_ids = [x["ID"] for x in self.clients]
         client_index = clients_ids.index(client_id)
         client = self.clients[client_index]
-        cipher = AES.new(client.get("PasswordHash"), AES.MODE_CBC)
-        ticket = self.generate_ticket(client_id, self.message_sever.get("uuid"), cipher)
-        return cipher, ticket
+        aes_key = AES.new(client.get("PasswordHash"), AES.MODE_CBC, iv=nonce)
+        ticket_payload = self.generate_ticket(client_id, server_id, aes_key)
+        return aes_key, self.encrypt_aes(aes_key, ticket_payload, nonce)
 
-    def generate_ticket(self, client_id, service_id, key):
+    def generate_ticket(self, client_id, server_id, key):
         """
-
-        :param key:
-        :param client_id:
-        :param service_id:
-        :return:
+        generate tgt from given key
+        :param server_id: message server id
+        :param key: AES Key used for encryption
+        :param client_id: client initiated request
+        :return: tgt
         """
+        creation_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        expiration_time = (datetime.now() + timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
         # ticket = f"{client_id}:{service_id}:{base64.b64encode(self.generate_salt()).decode()}"
-        ticket = f"{client_id}:{service_id}:{base64.b64encode(key).decode()}"
+        ticket = f"{self.version}:{client_id}:{server_id}"
+        ticket += f"{creation_time}:{base64.b64encode(key).decode()}:{expiration_time}"
         return ticket
 
     # TODO add support for multi servers using threads
@@ -163,7 +192,13 @@ class KerberosAuthServer:
                     }
                 )
                 add_client_to_file(self.clients)
-                return "Success!"
+                return {
+                    "Code": 1600,
+                    "Version": self.version,
+                    "Payload size": len(str(client_id)),
+                    "Payload": str(client_id)
+                }
+
         except Exception as e:
             print(str(e))
             return "Error! can't add user because of {}".format(str(e))
@@ -198,11 +233,55 @@ class KerberosAuthServer:
             code = request["Header"]["Code"]
             if code == 1024:
                 return self.register(request["Payload"])
+            if code == 1027:
+                print("Client requested key")
+                client_id = request["Header"]["ID"]
+                nonce = request["Payload"]["nonce"]
+                response = self.generate_session_key(client_id,
+                                                     self.message_sever.get("uuid"), nonce)
+                print("Created session key!")
+                print(json.dumps(dict(response)))
+                # TODO: check the encrypted key iv thing
+                encrypted_key = {
+                    "AES Key": self.encrypt_aes(response[0], nonce, response[0]),
+                    "Nonce": self.encrypt_aes(response[0], nonce, nonce),
+                    "Encrypted Key IV": ""
+                }
+                payload = {
+                    "encrypted_key": encrypted_key,
+                    "ticket": response[1]
+                }
+                print(f"Payload: \n{json.dumps(payload)}")
+                return {
+                    "Header": {
+                        "Code": 1603,
+                        "Version": self.version,
+                        "Payload Size": len(payload)
+                    },
+                    "Payload": payload
+                }
             return "Not supported yet!"
+        except KeyError as e:
+            print(f"Got Key Error on {e}")
+            exit(1)
         except Exception as e:
             print(str(e))
             exit(1)
 
+    def register_user(self):
+        client_name_for_request = self.receive_client_request()
+        if client_name_for_request:
+            client_request = {
+                "Header": {
+                    "Code": 1024,  # register code
+                    "Version": 24
+                },
+                "Payload": client_name_for_request
+            }
+            response = self.handle_client_request(client_request)
+            print(f"Server reply: {response}")
+            return response
+        return "Error"
     def start_server(self):
         """
         infinite loop of listening server
@@ -210,17 +289,28 @@ class KerberosAuthServer:
         """
         print(f"Server Started on port {self.port}")
         # client_request = self.receive_client_request()
-        client_name_for_request = self.receive_client_request()
-        if client_name_for_request:
-            client_request = {
-                "Header": {
-                    "Code": 1024, # register code
-                    "Version": 24
-                },
-                "Payload": client_name_for_request
+        response = self.register_user()
+        while "Error" in response:
+            response = self.register_user()
+
+        print("------------------------------------------")
+        client_id = response["Payload"]
+        client_request = {
+            "Header": {
+                "Code": 1027,  # register code
+                "Version": 24,
+                "ID": client_id
+            },
+            "Payload": {
+                "server_id": self.message_sever.get("uuid"),
+                "nonce": get_random_bytes(32)
             }
-            response = self.handle_client_request(client_request)
-            print(f"Server reply: {response}")
+        }
+        client_request["Header"]["Payload Size"] = len(client_request["Payload"])
+        print(json.dumps(client_request, indent=4, default=str))
+        response = self.handle_client_request(client_request)
+        print(f"Server reply: {response}")
+
 
         # while True:
         #     client_request = self.receive_client_request()
